5b7c6e6b34b1b725a0c79f4222438a51a347ac0d
Fritz Cedrick Villariza <villarizaced@gmail.com>
2021-10-31 12:02:26 +0800

    CODE COMPLETED: DEF FUNCTION

--- def-amount-and-price.py
+++ def-amount-and-price.py
@@ -3,18 +3,13 @@ print("\nWelcome to Villariza Foods! Our available product for today is apple. \
 
 #Added a docstring inside a def function so that the functionality of the system is accessible and easy to comprehend.
 def apple_change(money, apple):
-    """
-    Input from user and store-return as money and apple variable. The if statement shows that the user can afford an
-    apple based on the calculations that was inputted. However, else statement contradicts this estimate from the user.
-    """
-    if money >= apple:
-        exchange = float(money) % float(apple)
-        apple_maxQuantity = float(money) // float(apple)
+    if money >=  apple:
+        exchange = money % apple
+        apple_maxQuantity = money // apple
         print(f"\nYou can buy {apple_maxQuantity:,.0f} apple/s and your change is {exchange:,.2f} PHP.\n")
-        
-    elif money < apple:
-        moneyShortage = float(apple) - float(money)
+    else:
+        moneyShortage = apple - money
         print(f"\nSorry, but you do not have enough money to buy an apple. You need {moneyShortage:,.2f} PHP in order to purchase a single apple.\n")
 
 #Input string has added inside the defined function for a simpler collective method.
-apple_change(money = input("How much cash do you possess right now? \n> PHP: "), apple = input("\nWhat is the cost of an apple per item? \n> PHP: "))
+apple_change(money = float(input("How much cash do you possess right now? \n> PHP: ")), apple = float(input("\nWhat is the cost of an apple per item? \n> PHP: ")))
